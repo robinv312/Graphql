9965a000c29a9fb0df9f28a9a14fe5431573a8db
robin <robinv312@gmail.com>
2022-01-12 14:16:07 +0530

    query type

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,14 @@
+// const Query = {
+//     test: () => 'Test Success, GraphQL server is up & running !!'
+//  }
+//  module.exports = {Query}
+
+const db = require('./db')
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
- }
- module.exports = {Query}
+   greeting:() => {
+      return "hello from  TutorialsPoint !!!"
+   },
+   students:() => db.students.list()
+}
+
+module.exports = {Query}
